a3eb532b81ebc786b5c4372ec80700a2f3378220
m-e-h <info@martyhelmick.com>
2016-04-05 13:53:58 -0400

    add image size

--- includes/arch-width.php
+++ includes/arch-width.php
@@ -1,39 +1,60 @@
 <?php
+/**
+ * Width Grid Classes.
+ *
+ * @package  Arch Builder
+ */
 
 add_filter( 'post_class', 'arch_width_post_classes', 10, 3 );
 add_filter( 'hybrid_attr_content', 'arch_grid' );
 
+/**
+ * Adds custom classes to the array of post classes.
+ *
+ * @param array   $classes Classes for the post element.
+ * @param WP_Post $post_id The post object.
+ * @return array
+ */
+function arch_width_post_classes( $classes, $post_id ) {
 
-function arch_width_post_classes($classes, $class, $post_id) {
-
-if ( is_search() || is_single($post_id))
-	return;
+	if ( is_search() || is_single( $post_id ) ) {
+		return; }
 
 	$achive_width   = get_post_meta( $post_id, 'arch_width', true );
 	$arch_title     = get_post_meta( $post_id, 'arch_title', true );
 	$arch_component = get_post_meta( $post_id, 'arch_component', true );
 	$arch_height    = get_post_meta( $post_id, 'arch_height', true );
 
-	if ( $achive_width )
+	if ( $achive_width ) {
 		$classes[] = 'arch';
 		$classes[] = $achive_width;
+	}
 
-	if ( $arch_component )
+	if ( $arch_component ) {
 		$classes[] = $arch_component;
+	}
 
-	if ( $arch_title )
+	if ( $arch_title ) {
 		$classes[] = $arch_title;
+	}
 
-	if ( 'false' == $arch_height )
+	if ( 'false' === $arch_height ) {
 		$classes[] = 'u-flexed-start';
+	}
 
-    return $classes;
+	return $classes;
 }
 
-function arch_grid($attr) {
+/**
+ * Adds class to hybrid_attr_content.
+ *
+ * @since  2.0.0
+ * @param  array $attr Array of classes.
+ */
+function arch_grid( $attr ) {
 
-    if ( is_post_type_archive( arch_post_types() ) || arch_is_home() ) {
-        $attr['class']   .= " o-grid";
-    }
+	if ( is_post_type_archive( arch_post_types() ) || arch_is_home() ) {
+		$attr['class']   .= ' o-grid';
+	}
 	return $attr;
 }
--- templates/card.php
+++ templates/card.php
@@ -5,24 +5,24 @@
  * @package abraham
  */
 ?>
-<article <?php hybrid_attr('post'); ?>>
+<article <?php hybrid_attr( 'post' ); ?>>
 
-        <header <?php hybrid_attr('entry-header'); ?>>
-            <?php
-                get_the_image(array(
-                    //'size'               => 'abe-card-md',
-                    'image_class'        => 'u-1of1',
-                    'before'             => '<div class="card-img u-overflow-hidden">',
-                    'after'              => '</div>',
-                ));
-            ?>
-            
-                <?php arch_title(); ?>
+		<header <?php hybrid_attr( 'entry-header' ); ?>>
+			<?php
+				get_the_image(array(
+					'size'               => 'large',
+					'image_class'        => 'u-1of1',
+					'before'             => '<div class="card-img u-overflow-hidden">',
+					'after'              => '</div>',
+				));
+			?>
 
-        </header>
+				<?php arch_title(); ?>
 
-        <div <?php hybrid_attr('entry-summary'); ?>>
-            <?php arch_excerpt(); ?>
-        </div>
+		</header>
+
+		<div <?php hybrid_attr( 'entry-summary' ); ?>>
+			<?php arch_excerpt(); ?>
+		</div>
 
 </article>
