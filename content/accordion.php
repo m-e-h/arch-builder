2c4438a7c96556ba345092aad90b31357b1a42cc
m-e-h <info@martyhelmick.com>
2016-07-19 20:29:16 -0400

    better accordian with less markup

--- content/accordion.php
+++ content/accordion.php
@@ -26,10 +26,8 @@ wp_enqueue_script( 'arch-toggle' ); ?>
 	<a class="collapse-toggle arch-f-plus arch-px2 arch-py1 arch-1of1 arch-flex arch-flex-justify-between" data-collapse="#toggle<?php the_ID(); ?>" href="#"><?php the_title(); ?> <svg xmlns="http://www.w3.org/2000/svg" class="chevron-toggle" fill="currentcolor" viewBox="0 0 32 32"><path d="M1 12l15 14 15-14-4-4-11 10L5 8z"/></svg>
 	</a>
 
-	<div class="collapse" id="toggle<?php the_ID(); ?>">
-		<div class="collapse-wrap arch-px2 arch-pt1 arch-pb3">
+	<div class="collapse arch-p2" id="toggle<?php the_ID(); ?>">
 	    	<?php arch_excerpt(); ?>
-		</div>
 	</div>
 
 <?php endwhile; ?>
