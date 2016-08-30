1a8abd00c38a059a4d1f0c8a54aca3a8d4270b9a
m-e-h <info@martyhelmick.com>
2016-08-30 13:53:29 -0400

    default to false for arch home

--- includes/customizer.php
+++ includes/customizer.php
@@ -17,31 +17,17 @@ function arch_customize_register( $wp_customize ) {
 	// Setting: Checkbox.
 	$wp_customize->add_setting( 'arch_front_page', array(
 		'type'                 => 'theme_mod',
-		'default'              => 'enable',
+		'default'              => false,
 		'transport'            => 'refresh',
 		'capability'           => 'edit_theme_options',
-		'sanitize_callback'    => 'arch_validate_boolean' // Custom function in customizer-sanitization.php file.
+		'sanitize_callback'    => 'wp_validate_boolean'
 	) );
 	// Control: Checkbox.
 	$wp_customize->add_control( 'arch_front_page', array(
 		'label'       => __( 'Arch Front Page', 'arch' ),
 		'description' => __( 'Populate front-page with Arch Posts', 'arch' ),
 		'section'     => 'static_front_page',
-		'type'        => 'checkbox'
+		'type'        => 'checkbox',
 	) );
 
-}
-
-
-/**
- * Function for validating booleans before saving them as metadata. If the value is
- * `true`, we'll return a `1` to be stored as the meta value.  Else, we return `false`.
- *
- * @since  1.0.0
- * @access public
- * @param  mixed
- * @return bool|int
- */
-function arch_validate_boolean( $value ) {
-	return wp_validate_boolean( $value ) ? 1 : false;
 }
