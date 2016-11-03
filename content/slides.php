0726b40c218268d44e7aad89e770ba56b389c73e
m-e-h <info@martyhelmick.com>
2016-11-03 17:30:57 -0400

    format

--- content/slides.php
+++ content/slides.php
@@ -22,7 +22,7 @@ wp_enqueue_script( 'flickity' ); ?>
 				<?php
 			endforeach;
 
-		else:
+		else :
 		?>
 			<?php $query = new WP_Query(
 				array(
@@ -56,6 +56,6 @@ wp_enqueue_script( 'flickity' ); ?>
 		</div>
 	</div>
 
-	<?php edit_post_link('<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>'); ?>
+	<?php edit_post_link( '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>' ); ?>
 
 </div>
